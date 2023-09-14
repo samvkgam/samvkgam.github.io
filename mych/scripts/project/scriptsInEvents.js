e816412cdec89b1d58b4a48e221deec4dd9a2e67
samvkgams <127677321+samvkgams@users.noreply.github.com>
2023-09-14 17:43:29 +0300

    Add files via upload

---
+++ mych/scripts/project/scriptsInEvents.js
@@ -0,0 +1,19 @@
+
+
+
+const scriptsInEvents = {
+
+	async Main_Event4_Act2(runtime, localVars)
+	{
+		platformProvider.showRewarded()
+	},
+
+	async Main_Event5_Act2(runtime, localVars)
+	{
+		platformProvider.showInterstitial()
+	}
+
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
+
